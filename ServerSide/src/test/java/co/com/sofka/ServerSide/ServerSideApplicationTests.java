b68149323a1e22e94df02afd52cba2c22429e12d
francheskito <franccesco.giordano11@gmail.com>
2022-03-30 11:39:14 -0300

    Base inicial

---
+++ ServerSide/src/test/java/co/com/sofka/ServerSide/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.ServerSide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
